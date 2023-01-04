811cbcb8c11899c5f7fe5d835e4d9994927b32aa
dmitry <stalker2807sky@yandex.ru>
2023-01-04 20:00:47 +0400

    first commit

---
+++ declaration.d.ts
@@ -0,0 +1,17 @@
+﻿declare module "*.module.css" {
+  const exports: { [exportName: string]: string };
+  export = exports;
+}
+
+declare module "*.module.sass";
+
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
+
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
